f373df1458e5c3f146903c6058c440e5699be480
GabrielXN-est <g240254@dac.unicamp.br>
2025-09-14 17:07:43 -0300

    função para a plotagem dos resultados de cada modelo 1d

--- Work/Ex1.py
+++ Work/Ex1.py
@@ -5,6 +5,29 @@ import matplotlib.pyplot as plt
 
 import Rede_neural as rn
 
+def plotagem_1d_modelo(xf, func, y_pred, y_true, loss, func_label, path):
+    '''
+    xf: intervalo onde plotar a função
+    func: função exata
+    y_pred: valores preditos pelo modelo
+    y_true: valores de treinamento
+    loss: vetor com as perdas
+    func_label: texto a associar com função exata
+    path: caminho e nome do arquivo para salvar
+    '''
+    fig, (ax, ax2) = plt.subplots(2,1, figsize = (7, 10))
+    ax.plot(xf, func(xf), label = func_label)
+    ax.plot(xf, y_pred.detach().numpy(), label = "dados previstos")
+    ax.scatter(x_true, y_true, label = "valores de treinamento")
+    ax.legend()
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax2.set_title("Evolução da perda")
+    ax2.set_xlabel("epoch")
+    ax2.set_ylabel("loss")
+    ax2.plot(range(4000), loss)
+    fig.savefig(path)
+
 #função
 func = lambda x: x ** 3
 #amostras 
@@ -18,11 +41,22 @@ model = rn.neural_net_interno_1_hidden([1, 20, 1]) # uma camada interna de 20]
 loss = model.treino(x_true, y_true)
 
 xf = np.linspace(-6, 6, 12)
-a = t.tensor(xf, dtype = t.float32).T
 y_pred = model.foward(t.tensor(np.array([xf]).T, dtype = t.float32))
 
-plt.plot(xf, func(xf), label = "y = x³")
-plt.plot(xf, y_pred.detach().numpy(), label = "dados previstos")
-plt.scatter(x_true, y_true, label = "valores de treinamento")
-plt.legend()
-plt.show()
+error_measure = np.sum((func(xf)-y_pred.detach().numpy())**2)
+print(error_measure)
+'''
+fig, (ax, ax2) = plt.subplots(2,1, figsize = (7, 10))
+ax.plot(xf, func(xf), label = "y = x³")
+ax.plot(xf, y_pred.detach().numpy(), label = "dados previstos")
+ax.scatter(x_true, y_true, label = "valores de treinamento")
+ax.legend()
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax2.set_title("Evolução da perda")
+ax2.set_xlabel("epoch")
+ax2.set_ylabel("loss")
+ax2.plot(range(4000), loss)
+fig.savefig("Work\Ex1_graphs\Relu_hidden_1_20")
+'''
+plotagem_1d_modelo(xf, func, y_pred, y_true, loss, "y = x³", "Work\Ex1_graphs\Relu_hidden1_20")
