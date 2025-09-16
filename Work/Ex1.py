11c5b8a61e4b87578d2185075736e30d4acdad1e
GabrielXN-est <g240254@dac.unicamp.br>
2025-09-16 17:51:26 -0300

    Medição e gravação de erros

--- Work/Ex1.py
+++ Work/Ex1.py
@@ -28,6 +28,7 @@ def plotagem_1d_modelo(xf, func, y_pred, y_true, loss, func_label, path):
     ax2.set_ylabel("loss")
     ax2.plot(range(4000), loss)
     fig.savefig(path)
+
 #dicionário com os erros de diversas situações
 dict_erros = {}
 
@@ -40,6 +41,12 @@ xa, xb = -4, 4
 x_true = xa + (xb-xa) * t.rand(number_samples, 1)
 y_true = func(x_true)
 
+#valores de validação
+xf = np.linspace(-6, 6, 12)
+
+#erro de função
+error_func = t.nn.MSELoss()
+# error_func = lambda y_true, y_pred = np.sum((func(xf)-y_pred.detach().numpy())**2)
 for activation_func in ("ReLU", "sigmoid", "tanh"):
     dict_erros[activation_func] = {}
 
@@ -48,26 +55,25 @@ for activation_func in ("ReLU", "sigmoid", "tanh"):
         model = rn.neural_net_interno_1_hidden([1, neuronios_1, 1], activation_func) # uma camada interna de 20]
         loss = model.treino(x_true, y_true)
 
-        xf = np.linspace(-6, 6, 12)
-        y_pred = model.foward(t.tensor(np.array([xf]).T, dtype = t.float32))
+        y_pred = model.foward(t.tensor(np.array([xf]).T, dtype = t.float32)).squeeze()
 
-        error_measure = np.sum((func(xf)-y_pred.detach().numpy())**2)
+        error_measure = error_func(t.tensor(func(xf), dtype= t.float64), y_pred).item()
         dict_erros[activation_func][neuronios_1] = error_measure
 
         plotagem_1d_modelo(xf, func, y_pred, y_true, loss, "y = x³",
                             f"Work\Ex1_graphs\{activation_func}_hidden1_{neuronios_1}")
-        
+
     # 2 hidden layer
     for neuronios_1 in (5, 10, 15, 20, 25, 30):
         for neuronios_2 in (5, 10, 15, 20, 25, 30):
+            break
             model = rn.neural_net_interno_2_hidden([1, neuronios_1, neuronios_2, 1], activation_func) # uma camada interna de 20]
             loss = model.treino(x_true, y_true)
 
-            xf = np.linspace(-6, 6, 12)
-            y_pred = model.foward(t.tensor(np.array([xf]).T, dtype = t.float32))
+            y_pred = model.foward(t.tensor(np.array([xf]).T, dtype = t.float32)).squeeze()
 
-            error_measure = np.sum((func(xf)-y_pred.detach().numpy())**2)
-            dict_erros[activation_func][(neuronios_1, neuronios_2)] = error_measure
+            error_measure = error_func(t.tensor(func(xf), dtype= t.float64), y_pred).item()
+            dict_erros[activation_func][f"{neuronios_1},{neuronios_2}"] = error_measure
 
             plotagem_1d_modelo(xf, func, y_pred, y_true, loss, "y = x³",
                                 f"Work\Ex1_graphs\{activation_func}_hidden1_{neuronios_1}_hidden2_{neuronios_2}")
@@ -76,19 +82,17 @@ for activation_func in ("ReLU", "sigmoid", "tanh"):
     for neuronios_1 in (5, 10, 15, 20, 25, 30):
         for neuronios_2 in (5, 10, 15, 20, 25, 30):
             for neuronios_3 in (5, 10, 15, 20, 25, 30):
-                model = rn.neural_net_interno_2_hidden([1, neuronios_1, neuronios_2, neuronios_3, 1], activation_func) # uma camada interna de 20]
+                model = rn.neural_net_interno_3_hidden([1, neuronios_1, neuronios_2, neuronios_3, 1], activation_func) # uma camada interna de 20]
                 loss = model.treino(x_true, y_true)
 
-                xf = np.linspace(-6, 6, 12)
-                y_pred = model.foward(t.tensor(np.array([xf]).T, dtype = t.float32))
+                y_pred = model.foward(t.tensor(np.array([xf]).T, dtype = t.float32)).squeeze()
 
-                error_measure = np.sum((func(xf)-y_pred.detach().numpy())**2)
-                dict_erros[activation_func][(neuronios_1, neuronios_2, neuronios_3)] = error_measure
+                error_measure = error_func(t.tensor(func(xf), dtype= t.float64), y_pred).item()
+                dict_erros[activation_func][f"{neuronios_1},{neuronios_2},{neuronios_3}"] = error_measure
 
                 plotagem_1d_modelo(xf, func, y_pred, y_true, loss, "y = x³",
                                     f"Work\Ex1_graphs\{activation_func}_hidden1_{neuronios_1}_hidden2_{neuronios_2}_hidden3_{neuronios_3}")
-                
-#salvando erros
-#dict_erros = json.dumps(dict_erros)
-#with open("Work\Ex1_graphs\Erros.txt", "w") as file:
-#    print(dict_erros, file=file)
+
+dict_erros = json.dumps(dict_erros)
+with open("Work\Ex1_graphs\Erros.txt", "w") as file:
+    print(dict_erros, file=file)
